1df62766c464642ac75b5568f9063725ec1c8162
tadahiro1228 <70730178+tadahiro1228@users.noreply.github.com>
2020-09-11 10:57:04 +0900

    Update

--- src/components/Page1Body.jsx
+++ src/components/Page1Body.jsx
@@ -1,51 +1,16 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
-import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 
 import TabBar from "./TabBar";
-
-const TabPanel = (props) => {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-};
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired
-};
-
-function allyProps(index) {
-  return {
-    id: `full-width-tab-${index}`,
-    "aria-controls": `full-width-tabpanel-${index}`
-  };
-}
+import TabPanel from "./TabPanel";
 
 const Sheet1 = () => {
   return (
-    <React.Fragment>
-      <div>
-        <p>This is Icons site</p>
-      </div>
+    <div>
+      This is Icons site
+      <br />
       <a href="https://material-ui.com/components/material-icons/">Go site</a>
-    </React.Fragment>
+    </div>
   );
 };
 
@@ -73,21 +38,6 @@ const ButtonList = () => {
     </React.Fragment>
   );
 };
-
-// const TabBar = ({ value, onChange }) => {
-//   return (
-//     <React.Fragment>
-//       <h5>- Tab -</h5>
-//       <AppBar position="static">
-//         <Tabs value={value} onChange={onChange} variant="fullWidth">
-//           <Tab label="Tab 1" {...allyProps(0)} />
-//           <Tab label="Tab 2" {...allyProps(1)} />
-//           <Tab label="Tab 3" {...allyProps(2)} />
-//         </Tabs>
-//       </AppBar>
-//     </React.Fragment>
-//   );
-// };
 
 const Page1Body = () => {
   const [valTabs, setValTabs] = React.useState(0);
--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import "../styles.css";
 
@@ -32,10 +31,5 @@ class Page extends React.Component {
     );
   }
 }
-
-Page.propTypes = {
-  title: PropTypes.string.isRequored,
-  val: PropTypes.string.isRequired
-};
 
 export default Page;
